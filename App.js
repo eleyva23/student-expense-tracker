c01f1b5fb821d0e766ed3ae7fd62cbc9a1e47444
emma <eleyva2@usf.edu>
2025-11-23 18:34:44 -0800

    edited the create table function

--- App.js
+++ App.js
@@ -6,8 +6,20 @@ import ExpenseScreen from './ExpenseScreen';
 export default function App() {
   return (
     <SQLiteProvider databaseName="expenses_v2.db">
+      onInit={async (db) => {
+        await db.execAsync(`
+          CREATE TABLE IF NOT EXISTS expenses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            amount REAL NOT NULL,
+            date TEXT NOT NULL
+          );
+        `);
+      }}
+    
 
       <ExpenseScreen />
     </SQLiteProvider>
   );
 }
+
